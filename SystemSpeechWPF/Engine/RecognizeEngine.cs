c929d94913a57e6e2c791c3bca6ca65d1044ff20
Motoharu SOMEYA <maretol@outlook.com>
2018-01-15 19:20:32 +0900

    edit

--- SystemSpeechWPF/Engine/RecognizeEngine.cs
+++ SystemSpeechWPF/Engine/RecognizeEngine.cs
@@ -15,8 +15,48 @@ namespace SystemSpeechWPF.Engine
         public RecognizeEngine()
         {
             Engine = new SpeechRecognitionEngine(new CultureInfo("ja-jp"));
+            IsActive = false;
         }
 
+        /// <summary>
+        /// 動いてるかどうか
+        /// </summary>
+        public bool IsActive { get; private set; }
 
+        /// <summary>
+        /// 準備ができてるかどうか
+        /// </summary>
+        public bool IsReady
+        {
+            get
+            {
+                return !IsActive && false/*コマンドが入っているかどうか*/;
+            }
+        }
+
+        /// <summary>
+        /// 文法データ作って入れる
+        /// </summary>
+        private void CreateGrammar()
+        {
+
+        }
+
+        public void StartRecognize()
+        {
+            if (IsReady)
+            {
+                IsActive = true;
+                Task.Factory.StartNew(() => Engine.RecognizeAsync());
+            }
+        }
+
+        public void EndRecognize()
+        {
+            if (IsActive)
+            {
+                Engine.RecognizeAsyncStop();
+            }
+        }
     }
 }
