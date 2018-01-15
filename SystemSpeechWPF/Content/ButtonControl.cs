477f740295f7043d0afe3e888abfe21938e810a6
Motoharu SOMEYA <maretol@outlook.com>
2018-01-15 18:37:24 +0900

    add file

---
+++ SystemSpeechWPF/Content/ButtonControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SystemSpeechWPF.Content
+{
+    class ButtonControl : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Engineの状態で返す
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
