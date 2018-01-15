4d8556481f52fc3c0e23696fbf070319559cfb94
Motoharu SOMEYA <maretol@outlook.com>
2018-01-15 18:38:26 +0900

    change filename

---
+++ SystemSpeechWPF/Content/TextBoxData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemSpeechWPF.Content
+{
+    class TextBoxData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string commandList;
+        public string CommandList
+        {
+            get { return commandList; }
+            set { SetProperty(ref commandList, value); }
+        }
+
+        private string result;
+        public string Result
+        {
+            get { return result; }
+            set { SetProperty(ref result, value); }
+        }
+
+        public ButtonControl StartRecognize;
+
+
+    }
+}
